39ec601c1819c5e2719085b853da03fca8b1a70b
Anaxagore <julienguay@club-internet.fr>
2014-06-21 05:55:08 +0200

    Inversion des classes MemoryFileAccessor et ProcessObserver

--- MemoryHack/include/MemoryFileAccessor.h
+++ MemoryHack/include/MemoryFileAccessor.h
@@ -1,50 +1,14 @@
-#ifndef MEMORYFILEACCESSOR_H
-#define MEMORYFILEACCESSOR_H
+#ifndef MemoryFileAccessor_H
+#define MemoryFileAccessor_H
 
-#include <iostream>
-#include <fstream>
-#include <windows.h>
-#include <psapi.h>
-
-using namespace std;
 
 class MemoryFileAccessor
 {
     public:
-        MemoryFileAccessor(const int begin,const int end,const string& name);
+        MemoryFileAccessor();
         virtual ~MemoryFileAccessor();
-
-        /** \brief Copie une portion de la RAM dans file_pointer_
-         *
-         * Copie la RAM de à partir de l'adresse beginning_adress_ jusqu'à ending_address_
-         * dans le fichier file_name_ qui est céé à cette occasion.
-         *
-         */
-        void copyMemoryInFile();
-
-        /** \brief Permet un accès direct à n'importe quel octet de la copie de la mémoire
-        /
-        /   Retourne l'octet dont l'adresse est address dans la capture mémoire stockée dans file_pointer_
-        /
-        */
-        byte operator[](const DWORD address);
-
-        /** \brief Retourne la plus grande valeur adressable
-        /
-        /   Retourne la plus grande valeur adressable, c'est à dire la working set size de FTL -1
-        /
-        */
-        DWORD getMaxAdress();
-
     protected:
     private:
-        DWORD beginning_address_;
-        DWORD ending_address_;
-        int size_;
-        string file_name_;
-
-
-
 };
 
-#endif // MEMORYFILEACCESSOR_H
+#endif // MemoryFileAccessor_H
--- MemoryHack/src/ProcessObserver.cpp
+++ MemoryHack/src/ProcessObserver.cpp
@@ -1,2 +1,86 @@
 #include "ProcessObserver.h"
 
+
+ProcessObserver::~ProcessObserver()
+{
+    remove(file_name_.c_str());
+}
+
+ProcessObserver::ProcessObserver(const int begin,const int end,const string& name):beginning_address_(begin),ending_address_(end),size_(end-begin),file_name_(name)
+{
+}
+
+void ProcessObserver::copyMemoryInFile()
+{
+    byte value = 0;
+    DWORD pid;
+    HWND hwnd;
+    hwnd = FindWindow(NULL,"FTL");//Rentré en dur comme un gros porc
+    ofstream file(file_name_, ios::out | ios::trunc | ios::binary);  //On ouvre le fichier en écriture
+    if(!hwnd)
+    {
+        cout<<"La fenêtre n'a pas été trouvée"<<endl;
+        exit(1);//Dans l'idéal il faudrait lancer une exception
+    }
+    if(!file)//Si le fichier ne s'est pas bien ouvert
+    {
+        cout<<"Le fichier ne s'est pas ouvert correctement"<<endl;
+        exit(2);//Dans l'idéal il faudrait lancer une exception
+    }
+    GetWindowThreadProcessId(hwnd,&pid);
+    HANDLE phandle = OpenProcess(PROCESS_QUERY_INFORMATION,0,pid);
+    if(!phandle)
+    {
+        cout<<"Impossible d'obtenir la poignée (handle)"<<endl;
+        exit(3);//Dans l'idéal il faudrait lancer une exception
+    }
+    for(DWORD address=beginning_address_; address<=ending_address_; address++)
+    {
+        ReadProcessMemory(phandle,(void*)address,&value,sizeof(value),0);
+        file.write((char*)&value, sizeof(byte));
+    }
+    CloseHandle(phandle);
+    file.close();
+}
+
+byte ProcessObserver::operator[](const DWORD address)
+{
+    byte value=0;
+    ifstream file(file_name_, ios::in| ios::binary);
+    if(!file)
+    {
+        cout<<"Le fichier ne s'est pas ouvert correctement"<<endl;
+        exit(2);//Dans l'idéal il faudrait lancer une exception
+    }
+    file.seekg(address-this->beginning_address_);
+    file.read((char*)&value, sizeof(byte));
+    file.close();
+    return value;
+}
+
+DWORD ProcessObserver::getMaxAdress()
+{
+    DWORD pid, psize;
+    PROCESS_MEMORY_COUNTERS counters;
+    HWND hwnd;
+    hwnd = FindWindow(NULL,"Démineur");
+    if(!hwnd)
+    {
+        cout <<"Window not found!\n";
+        exit(1);
+    }
+    GetWindowThreadProcessId(hwnd,&pid);
+    HANDLE phandle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ,0,pid);
+    if(!phandle)
+    {
+        cout <<"Could not get handle!\n";
+        exit(1);
+    }
+    if(!GetProcessMemoryInfo(phandle,&counters, sizeof(counters)))
+    {
+        cout <<"Erreur : getProcessMemoryInfo\n";
+        exit(1);
+    }
+    psize = counters.PagefileUsage;
+    return psize;
+}
