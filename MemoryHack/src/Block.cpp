613c6d71595918badab7a14ef9b9dfe1f39cc007
Anaxagore <julienguay@club-internet.fr>
2014-06-25 14:35:13 +0200

    Block.cpp

--- MemoryHack/src/Block.cpp
+++ MemoryHack/src/Block.cpp
@@ -1,28 +1,32 @@
 #include "Block.h"
 
-Block::Block(DWORD begin, DWORD end, BlockType type):begin_(begin),end_(end),type_(type),size_((long)(end - begin +1)), subblocks_(vector<Block*>())
+template<typename T> Block<T>::Block(DWORD begin, DWORD end, BlockType type, T mem_access):begin_(begin),end_(end),type_(type), mem_accessor_(mem_access), size_((long)(end - begin +1)), subblocks_(vector<Block*>())
 {
 }
 
-Block::~Block()
+template<typename T> Block<T>::~Block()
 {
-    vector<Block*>::iterator i(subblocks_.begin());
-    while(i!=subblocks_.end()){
+    typename vector<Block<T>*>::iterator i(subblocks_.begin());
+    while(i!=subblocks_.end())
+    {
         delete *i;
     }
 }
 
-Block& Block::getBlock(int indice) const{
+template<typename T> Block<T>& Block<T>::getBlock(int indice) const
+{
     return *subblocks_[indice];
 }
 
-Block& Block::getBlock(Block::BlockType type, int indice) const{
+template<typename T> Block<T>& Block<T>::getBlock(Block::BlockType type, int indice) const
+{
     int count = 0;
 
     for(auto it(subblocks_.begin()) ; it != subblocks_.end() ; it++)
     {
         //Erreur de compil
-        if((*it)->getType == type){
+        if((*it)->getType == type)
+        {
             if(count == indice)
                 return **it;
             else
@@ -40,15 +44,19 @@ Block& Block::getBlock(Block::BlockType type, int indice) const{
 //    }
 //    Block* extractAndClean(DWORD begin, DWORD end) const{
 //    }
-    long Block::getSize(){
-        return size_;
-    }
-    DWORD Block::getBegin(){
-        return begin_;
-    }
-    DWORD Block::getEnd(){
-        return end_;
-    }
-    Block::BlockType Block::getType(){
-        return type_;
-    }
+template<typename T> long Block<T>::getSize()
+{
+    return size_;
+}
+template<typename T> DWORD Block<T>::getBegin()
+{
+    return begin_;
+}
+template<typename T> DWORD Block<T>::getEnd()
+{
+    return end_;
+}
+template<typename T> typename Block<T>::BlockType Block<T>::getType()
+{
+    return type_;
+}
